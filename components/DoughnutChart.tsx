1a24f6e0a09912510b9b7f091f634cf4b657ee11
Lancelot4306 <lancelot4306@gmail.com>
2026-02-03 12:30:03 +0530

    Fix doughnut chart

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -6,33 +6,32 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
-	const accountNames = accounts.map((a) => a.name);
-  	const balances = accounts.map((a) => a.currentBalance)
+  const accountData = Array.isArray(accounts[0]) ? accounts[0] : accounts;
+  const accountNames = accountData.map((a: Account) => a.name);
+  const balances = accountData.map((a: Account) => a.currentBalance);
 
-	const data = {
-		datasets: [
-			{
-				label: 'Banks',
-				data: balances,
-				backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
-			}
-		],
-		labels: accountNames
-	};
+  const data = {
+    datasets: [
+      {
+        label: 'Banks',
+        data: balances,
+        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
+      }
+    ],
+    labels: accountNames
+  }
 
-	return (
-		<Doughnut 
-			data={data} 
-			options={{
-				cutout: '55%',
-				plugins: {
-					legend: { 
-						display: false 
-					}
-				}
-			}}
-		/>
-	)
+  return <Doughnut 
+    data={data} 
+    options={{
+      cutout: '60%',
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }}
+  />
 }
 
 export default DoughnutChart
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,29 +1,29 @@
-import AnimatedCounter from './AnimatedCounter'
-import DoughnutChart from './DoughnutChart'
+import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
 
-const TotalBalanceBox = ({ accounts=[], totalBanks, totalCurrentBalance }: TotalBalanceBoxProps) => {
+const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotalBalanceBoxProps) => {
+	const flatAccounts = Array.isArray(accounts[0]) ? accounts[0] : accounts;
   return (
-	<section className='total-balance'>
-			<div className='total-balance-chart'>
-				<DoughnutChart accounts={accounts} />
-			</div>
+    <section className="total-balance">
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={flatAccounts} />
+      </div>
 
-			<div className='flex flex-col gap-6'>
-				<h2 className='header-2'>
-					Bank Accounts: {totalBanks} 
-				</h2>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">
+          Bank Accounts: {totalBanks}
+        </h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">
+            Total Current Balance
+          </p>
 
-				<div className='flex flex-col gap-2'>
-					<p className='total-balance-label'>
-						Total Current Balance
-					</p>
-
-					<div className='total-balance-amount flex-center gadiv-2'>
-						<AnimatedCounter amount={totalCurrentBalance} />
-					</div>
-				</div>
-			</div>
-		</section>
+          <div className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
